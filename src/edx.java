422d72261377d3eefeb1d9bf5ab5573bb253e3f7
Andrea Garcia <andreagarcia@andreas-mbp.attlocal.net>
2018-08-10 21:28:30 -0500

    first commit

---
+++ src/edx.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class edx {
+    /*Project 1
+    * International Travel Planner
+    */
+
+    public static void main(String [] args) {
+        meetAndGreet();
+        details();
+    }
+
+    public static void meetAndGreet(){
+        // * Ask user name
+        System.out.print("Hello, what is your name?");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println(name + ",please sit back and relax. You will now experience how planning for air " +
+                "travel can be effortless.");
+    }
+
+    //Part 2 – Travel time and Budget
+    public static void details(){
+        System.out.print("How many days would you like to be on your trip?");
+        Scanner daysInput = new Scanner(System.in);
+        String numberOfDaysTraveling = daysInput.nextLine();
+        System.out.print("You will spend " + numberOfDaysTraveling +
+                " days, hours,and minutes, and seconds at your destination");
+
+        System.out.print("What is your total budget for the trip in USD?");
+        Scanner budgetInput = new Scanner(System.in);
+        String budgetAmount = budgetInput.nextLine();
+        System.out.print("Your budget in US Dollars is: "+
+                budgetAmount + " and you can spend " + budgetAmount + " per day.");
+
+        System.out.print("What is the currency symbol for your destination?");
+        Scanner currencyCodeInput = new Scanner(System.in);
+        String currencyCode = currencyCodeInput.nextLine();
+        System.out.print("What is the conversion rate for the destination?");
+        Scanner currencyRateInput = new Scanner(System.in);
+        String currencyRate = currencyRateInput.nextLine();
+        String transformedBudget = currencyRate * budgetAmount;
+        System.out.print("Your budget in the destination's currency is" +
+                transformedBudget + "for the entire trip and" +
+                transformedBudget + "per day");
+
+    }
+
+}
