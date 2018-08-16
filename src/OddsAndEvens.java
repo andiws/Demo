2a87c561b6fb0f35525c9985f79117f1ae68f45c
Andrea Garcia <andreagarcia@andreas-mbp.attlocal.net>
2018-08-15 22:36:12 -0500

    Correcting sequence

--- src/OddsAndEvens.java
+++ src/OddsAndEvens.java
@@ -1,41 +1,42 @@
 import java.util.Scanner;
 
 public class OddsAndEvens {
-    public static void main (String [] args)
-    {
-    System.out.println("Let’s play a game called “Odds and Evens”");
-
-    System.out.println("What is your name?");
-
-    Scanner input = new Scanner(System.in);
-
-    String answer = input.next();
-
-    System.out.println("Hi " + answer + ", which do you choose? (O)dds or (E)vens?");
-
-    Scanner answerChoiceInput = new Scanner(System.in);
-
-    String answerChoice = answerChoiceInput.next();
-
-        int i = 0;
-        do {
-        if (answerChoice.equalsIgnoreCase("e")) {
-            String xyz = "Odd";
-
-            System.out.println(answer + " has picked " + answerChoice +
-                    "! The computer will be " + xyz + ".");
-
-        } else if (answerChoice.equalsIgnoreCase("o")) {
-            System.out.println(answer + " has picked " + answerChoice +
-                    "! The computer will be even.");
-
-        }
+    public static void main(String[] args) {
 
 
-            System.out.println ("--------------------------------------------------");
-        } while (i > 20);
+
+            System.out.println("Let’s play a game called “Odds and Evens”");
+
+            System.out.println("What is your name?");
+
+            Scanner input = new Scanner(System.in);
+
+            String answer = input.next();
+
+            while (answer != "") {
+                System.out.println("Hi " + answer + ", which do you choose? (O)dds or (E)vens?");
+
+                Scanner answerChoiceInput = new Scanner(System.in);
+
+                String answerChoice = answerChoiceInput.next();
+
+
+                if (answerChoice.equalsIgnoreCase("e")) {
+                    String xyz = "Odds";
+
+                    System.out.println(answer + " has picked " + answerChoice +
+                            "! The computer will be " + xyz + ".");
+                    System.out.println("--------------------------------------------------");
+
+
+                } else if (answerChoice.equalsIgnoreCase("o")) {
+                    System.out.println(answer + " has picked " + answerChoice +
+                            "! The computer will be evens.");
+                    System.out.println("--------------------------------------------------");
+
+
+                }
+            }
     }
-
-
-    }
+}
 
