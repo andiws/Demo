2317b97b7dcfc2989efc5657eb9ba9120d383fe1
Andrea Garcia <andreagarcia@andreas-mbp.attlocal.net>
2018-08-20 21:29:53 -0500

    Step two of game without modulus

--- src/OddsAndEvens.java
+++ src/OddsAndEvens.java
@@ -1,42 +1,64 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class OddsAndEvens {
     public static void main(String[] args) {
 
 
+        System.out.println("Let’s play a game called “Odds and Evens”");
 
-            System.out.println("Let’s play a game called “Odds and Evens”");
+        System.out.println("What is your name?");
 
-            System.out.println("What is your name?");
+        Scanner input = new Scanner(System.in);
 
-            Scanner input = new Scanner(System.in);
+        String answer = input.next();
 
-            String answer = input.next();
+        System.out.println("Hi " + answer + ", which do you choose? (O)dds or (E)vens?");
 
-            while (answer != "") {
-                System.out.println("Hi " + answer + ", which do you choose? (O)dds or (E)vens?");
+        Scanner answerChoiceInput = new Scanner(System.in);
 
-                Scanner answerChoiceInput = new Scanner(System.in);
+        String answerChoice = answerChoiceInput.next();
 
-                String answerChoice = answerChoiceInput.next();
-
-
-                if (answerChoice.equalsIgnoreCase("e")) {
-                    String xyz = "Odds";
-
-                    System.out.println(answer + " has picked " + answerChoice +
-                            "! The computer will be " + xyz + ".");
-                    System.out.println("--------------------------------------------------");
+        while (answer.equalsIgnoreCase("e") || answer.equalsIgnoreCase("o")) {
 
 
-                } else if (answerChoice.equalsIgnoreCase("o")) {
-                    System.out.println(answer + " has picked " + answerChoice +
-                            "! The computer will be evens.");
-                    System.out.println("--------------------------------------------------");
+            if (answerChoice.equalsIgnoreCase("e")) {
+                String xyz = "Odds";
+
+                System.out.println(answer + " has picked " + answerChoice +
+                        "! The computer will be " + xyz + ".");
+                System.out.println("--------------------------------------------------");
 
 
-                }
+            } else if (answerChoice.equalsIgnoreCase("o")) {
+                System.out.println(answer + " has picked " + answerChoice +
+                        "! The computer will be evens.");
+                System.out.println("--------------------------------------------------");
+
+
             }
+        }
+
+        System.out.println("How many fingers do you put out?");
+
+        Scanner userQuestion = new Scanner(System.in);
+
+        String userNumber = input.next();
+
+
+        Random rand = new Random();
+
+        int computerNumber = rand.nextInt(6);
+
+        System.out.println("The computer plays number fingers.");
+
+        System.out.println("--------------------------------------------------");
+
+
+        String sum = userNumber + computerNumber;
+
+        System.out.println(userNumber + "," + computerNumber + sum);
+
+
     }
 }
 
